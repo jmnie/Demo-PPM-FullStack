cd991d93fcfbeb1e8bda35e7fcb827bb52bc1d2d
jmnie <jiaming.nie13@gmail.com>
2020-08-03 08:57:47 +0800

    8-2

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css"
+import {BrowserRouter as Router, Route} from "react-router-dom"
+import AddProject from './components/Project/AddProject';
 
 class App extends Component{
   render(){
   return (
+    <Router>
       <div className="App">
-      <Dashboard />
+        <Header />
+        <Dashboard />
+      <Route exact path="/addProject" Component={AddProject}/>
       </div>
+    </Router>
     );
   }
 }
